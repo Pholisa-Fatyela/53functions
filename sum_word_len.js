a470b52e9979df3b21173166dc14eca1f274285b
Pholisa <pholisa@projectcodex.co>
2015-03-19 16:43:10 +0200

    The word length sum function

---
+++ sum_word_len.js
@@ -0,0 +1,8 @@
+function sum_word_len(str) {
+	var str = "hello my name is pholisa fatyela";
+    var splitStr = str.split(" ");
+    var joinStr = splitStr.join("");
+    var strLen = joinStr.length;
+
+    return strLen;
+}
