55a00dbc9b7acc7af7104680f5f9444cd7412df0
Pholisa <pholisa@projectcodex.co>
2015-03-19 16:38:54 +0200

    The high low function, the second approach to the problem

---
+++ high_low2.js
@@ -0,0 +1,19 @@
+function arrayMin(arr) {
+  var len = arr.length, min = Infinity;
+  while (len--) {
+    if (Number(arr[len]) < min) {
+      min = Number(arr[len]);
+    }
+  }
+  return min;
+};
+
+function arrayMax(arr) {
+  var len = arr.length, max = -Infinity;
+  while (len--) {
+    if (Number(arr[len]) > max) {
+      max = Number(arr[len]);
+    }
+  }
+  return max;
+};
