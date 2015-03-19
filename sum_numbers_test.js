b76b567fa7f10293f2bf4b6eb2d2f9e0260b63f9
Pholisa <pholisa@projectcodex.co>
2015-03-19 16:42:40 +0200

    The sum numbers function test

---
+++ sum_numbers_test.js
@@ -0,0 +1,5 @@
+TestMyCode.run("testing sum numbers function", function(assert){
+    var result = sum_numbers();
+    // is the result as we expected?
+    assert.equals("hello world!", result);
+});
