695e67b8d010199361f2ac3339e525e51d908603
Pholisa-Fatyela <pholisa@projectcodex.co>
2016-09-19 15:20:57 +0200

    ????

--- reverse.js
+++ reverse.js
@@ -1,10 +1,10 @@
-function reverse(str) {
-	var i = str.length, 
-	newStr = "";
+function Reverse(str) {
+	var i = str.length;
+	var newStr = "";
 
 	while(i > 0) {
-		newStr += str.substring(i-1,i);
+		newStr += str.substring(i-1, i);
 		i--;
-	}
+	} 
 	return newStr;
 }
